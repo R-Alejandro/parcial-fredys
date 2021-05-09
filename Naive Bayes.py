966d366907efc2d545f1bdfac75585515f545220
joseph <joseph.2210@hotmail.es>
2021-05-09 03:11:07 -0500

    naive bayes

--- Naive Bayes.py
+++ Naive Bayes.py
@@ -8,19 +8,19 @@ import pandas as pd
 datos = pd.read_csv("CryptoCoins.csv")
 # filas, columnas
 X = datos.iloc[:20,[9,10]].values #Open
-y = model.predict(X)
+y = GaussianNB()
 plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
 plt.show()
 
-model = GaussianNB()
-model.fit(X, y)
-rng = np.random.RandomState(0)
-Xnew = [-6, -14] + [14, 18] * rng.rand(2000, 2)
-ynew = model.predict(Xnew)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
-lim = plt.axis()
-plt.scatter(Xnew[:, 0], Xnew[:, 1], c=ynew, s=20, cmap='RdBu', alpha=0.1)
-plt.axis(lim);
-yprob = model.predict_proba(Xnew)
-yprob[-8:].round(2)
-print(yprob)
+# model = GaussianNB()
+# model.fit(X, y)
+# rng = np.random.RandomState(0)
+# Xnew = [-6, -14] + [14, 18] * rng.rand(2000, 2)
+# ynew = model.predict(Xnew)
+# plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='RdBu')
+# lim = plt.axis()
+# plt.scatter(Xnew[:, 0], Xnew[:, 1], c=ynew, s=20, cmap='RdBu', alpha=0.1)
+# plt.axis(lim);
+# yprob = model.predict_proba(Xnew)
+# yprob[-8:].round(2)
+# print(yprob)
